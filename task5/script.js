17d11288bfe20cd05bcf0d092abf3e169e0e74fb
Krillof <artem.krilof2002@mail.ru>
2022-10-11 07:30:44 +0300

    3

--- task5/script.js
+++ task5/script.js
@@ -1,24 +1,29 @@
 
 
-function main(){
+function answer_click_event() {
+    var price_input = document.getElementById("price_input");
+    var amount_input = document.getElementById("amount_input");
+    var answer_element = document.getElementById("answer_element");
+
+    var check_is_number = /^[0-9]+$/;
+
+    var check_a = check_is_number.test(price_input.value);
+    var check_b = check_is_number.test(amount_input.value);
+    var wrong_data = "Неправильный формат ввода! Вводить только цифры!";
+
+    var ans = 0;
+
+    if (check_a && check_b) {
+        ans = (Number(price_input.value) + Number(amount_input.value));
+        answer_element.innerText = "Ответ: " + ans;
+    } else {
+        answer_element.innerText = wrong_data;
+    }
+}
+
+function main() {
     var make_answer_button = document.getElementById("make_answer_button");
-
-    make_answer_button.addEventListener("click", ()=>{
-        var price_input = document.getElementById("price_input");
-        var amount_input =  document.getElementById("amount_input");
-        var answer_element = document.getElementById("answer_element");
-
-        var check_is_number = /^[0-9]+$/;
-
-        if (check_is_number.test(price_input.value) 
-            && check_is_number.test(amount_input.value)){
-                var a = Number(price_input.value);
-                var b = Number(amount_input.value);
-                answer_element.innerText = "Ответ: " + (a + b);
-            } else {
-                alert("Неправильный формат ввода! Вводить только цифры!");
-            }
-    })
+    make_answer_button.addEventListener("click", answer_click_event);
 
 }
 
