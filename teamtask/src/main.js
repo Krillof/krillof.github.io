6e2d1900cdaf2809fd073474b650111f8f7b79c8
Krillof <artem.krilof2002@mail.ru>
2023-01-23 15:09:49 +0300

    2

--- teamtask/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import store from './store.js'
-import App from './App.vue'
-import jQuery from 'jquery'
-window.$ = window.jQuery = jQuery
-
-createApp(App).use(store).mount('#app')
